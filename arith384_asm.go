2b08b6a35a425a6dd15b5ae7e816e870c9d3c927
Jared Wasinger <j-wasinger@hotmail.com>
2021-04-22 19:34:02 +0000

    compiles and passes tests

--- arith384_asm.go
+++ arith384_asm.go
@@ -1,30 +1,36 @@
 package mont_arith
 
 import (
-	"unsafe"
 	"github.com/jwasinger/mont-arith/arith384_asm"
+	"unsafe"
 )
 
-func MulModMont384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) (error) {
-	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))[:]
-	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))[:]
-	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))[:]
-	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))[:]
-	arith384_asm.MulMod(z, x, y, mod, ctx.MontParamInterleaved)
+func MulModMont384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) error {
+	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))
+	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))
+	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))
+	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))
+	arith384_asm.MulMod384(z, x, y, mod, ctx.MontParamInterleaved)
+
+	return nil
 }
 
-func AddMod384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) (error) {
-	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))[:]
-	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))[:]
-	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))[:]
-	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))[:]
-	arith384_asm.AddMod(z, x, y, mod)
+func AddMod384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) error {
+	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))
+	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))
+	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))
+	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))
+	arith384_asm.AddMod384(z, x, y, mod)
+
+	return nil
 }
 
-func SubMod384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) (error) {
-	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))[:]
-	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))[:]
-	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))[:]
-	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))[:]
-	arith384_asm.SubMod(z, x, y, mod)
+func SubMod384_asm(out_bytes, x_bytes, y_bytes []byte, ctx *MontArithContext) error {
+	x := (*[6]uint64)(unsafe.Pointer(&x_bytes[0]))
+	y := (*[6]uint64)(unsafe.Pointer(&y_bytes[0]))
+	z := (*[6]uint64)(unsafe.Pointer(&out_bytes[0]))
+	mod := (*[6]uint64)(unsafe.Pointer(&ctx.Modulus[0]))
+	arith384_asm.SubMod384(z, x, y, mod)
+
+	return nil
 }
